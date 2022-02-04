5cbab4e03583aa877e040a9cdc4f8e6abecfb583
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-02-04 21:33:14 +0530

    Change indentation width

--- Formula/shtkc.rb
+++ Formula/shtkc.rb
@@ -1,19 +1,19 @@
 class Shtkc < Formula
-    depends_on "llvm@12"
+  depends_on "llvm@12"
 
-    desc "ShnooTalk is a new programming language"
-    homepage "https://github.com/RainingComputers/shnootalk"
-    url "https://github.com/RainingComputers/ShnooTalk/archive/refs/tags/0.1.0-alpha.tar.gz"
-    sha256 "b4403de5fdae222d1a583b7b5279cafeb5e9254365f3319e76af61ad92cdfbbb"
-    license "MIT"
+  desc "ShnooTalk is a new programming language"
+  homepage "https://github.com/RainingComputers/shnootalk"
+  url "https://github.com/RainingComputers/ShnooTalk/archive/refs/tags/0.1.0-alpha.tar.gz"
+  sha256 "b4403de5fdae222d1a583b7b5279cafeb5e9254365f3319e76af61ad92cdfbbb"
+  license "MIT"
   
-    def install   
-        system "make", "build", "-j", "4"
-        build_name = File.read("build-name.txt").strip
-        bin.install "bin/#{build_name}/shtkc"
-    end
+  def install   
+    system "make", "build", "-j", "4"
+    build_name = File.read("build-name.txt").strip
+    bin.install "bin/#{build_name}/shtkc"
+  end
   
-    test do
-      system "shtkc", "-version"
-    end
+  test do
+    system "shtkc", "-version"
+  end
 end
