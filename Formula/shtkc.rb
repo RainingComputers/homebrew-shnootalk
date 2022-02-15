daed0ef3171541987d479a5075b12242821e5f30
Vishnu Shankar <vishnu.vish.shankar@gmail.com>
2022-02-15 18:26:52 +0530

    Update shtkc.rb

--- Formula/shtkc.rb
+++ Formula/shtkc.rb
@@ -1,8 +1,8 @@
 class Shtkc < Formula
   desc "ShnooTalk is a new programming language"
   homepage "https://github.com/RainingComputers/shnootalk"
-  url "https://github.com/RainingComputers/ShnooTalk/archive/refs/tags/0.1.0-alpha.tar.gz"
-  sha256 "b4403de5fdae222d1a583b7b5279cafeb5e9254365f3319e76af61ad92cdfbbb"
+  url "https://github.com/RainingComputers/ShnooTalk/archive/refs/tags/0.1.1-alpha.tar.gz"
+  sha256 "54aee149273a4a728f7a4e618a623e5d9b733998f81389091d1f12b818b05c83"
   license "MIT"
 
   depends_on "llvm@12"
